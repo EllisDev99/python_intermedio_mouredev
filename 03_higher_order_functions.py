85503cb58b89513b198fc11d7ca337555e8cd31c
Brayan Ellis <ellis@localhost.localdomain>
2024-12-04 22:17:56 -0400

    feat: agregamos la función sum_five y modificamos la función sum_two_values para que reciba como parametro una variable a la que se le asigne una función

---
+++ 03_higher_order_functions.py
@@ -0,0 +1,14 @@
+### Funciones de orden superior ###
+### Higher Order Functions ###
+
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+
+def sum_two_values_and_value(first_value, second_value, f_sum):
+    return f_sum(first_value + second_value)
+
+print(sum_two_values_and_value(5, 2, sum_one))
+print(sum_two_values_and_value(5, 2, sum_five))
