7199cb85732d72cccf70b9641b0fec6d9a5701ca
Brayan Ellis <ellis@localhost.localdomain>
2024-12-17 16:14:55 -0400

    feat: pruebas de lectura finalizadas

--- 05_file_handling.py
+++ 05_file_handling.py
@@ -1,1 +1,13 @@
 ### File Handling ###
+
+import os
+
+# .txt file
+
+txt_file = open('my_file.txt', 'r+') # Leer y escribir
+#print(txt_file.read()) # Muestra por consola el contenido
+#print(txt_file.read(10)) # Muestra los primeros 10 caracteres
+#print(txt_file.readline()) # Muestra la primera línea
+#print(txt_file.readlines()) # Muestra todas las líneas dentro de una lista
+
+txt_file.write('\nAunque también me interesa Rust')
