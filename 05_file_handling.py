e6b76766a3a75376773bc0629cd6a6251ea5a36b
Brayan Ellis <ellis@localhost.localdomain>
2024-12-18 17:56:39 -0400

    feat: terminamos con los .txt, comensamos con .json

--- 05_file_handling.py
+++ 05_file_handling.py
@@ -10,4 +10,8 @@ txt_file = open('my_file.txt', 'r+') # Leer y escribir
 #print(txt_file.readline()) # Muestra la primera línea
 #print(txt_file.readlines()) # Muestra todas las líneas dentro de una lista
 
+
+txt_file = open('my_file.txt', 'a') # Abre el archivo para agregar al final de todo
 txt_file.write('\nAunque también me interesa Rust')
+
+# .json
