c50afb4fbeea7070e0889241b9633fb741bceb18
Brayan Ellis <ellis@localhost.localdomain>
2024-12-19 09:43:23 -0400

    feat: Terminando el tema de los .json

--- 05_file_handling.py
+++ 05_file_handling.py
@@ -10,8 +10,30 @@ txt_file = open('my_file.txt', 'r+') # Leer y escribir
 #print(txt_file.readline()) # Muestra la primera línea
 #print(txt_file.readlines()) # Muestra todas las líneas dentro de una lista
 
-
 txt_file = open('my_file.txt', 'a') # Abre el archivo para agregar al final de todo
-txt_file.write('\nAunque también me interesa Rust')
+#txt_file.write('\nAunque también me interesa Rust')
 
-# .json
+
+# .json file
+
+import json
+
+json_file = open('my_file.json', 'w+')
+
+json_test = {
+    'name':'Brayan',
+    'subname':'Medina',
+    'age':25,
+    'languages':['Python', 'Rust', 'Kotlin'],
+    'email':'emailtest_1@gmail.com'
+}
+
+json.dump(json_test, json_file, indent = 4)
+
+json_file.close()
+
+
+json_dict = json.load(open('my_file.json'))
+print(type(json_dict))
+print(json_dict)
+print(json_dict['languages'])
