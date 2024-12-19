156816a44459713326ed1fbc3561f1c42e28ec75
Brayan Ellis <ellis@localhost.localdomain>
2024-12-19 15:23:37 -0400

    feat: agregando definiciones

--- 06_regular_expressions.py
+++ 06_regular_expressions.py
@@ -15,7 +15,10 @@ title('match')
 my_string = 'Esta es la lección número 6: \nLección llamada Expresiones regulares'
 my_other_string = 'Esta no es la lección número 6: Manejo de ficheros'
 
-# re.match busca el patrón al principio de la cadena, en caso de no estar retorna None
+"""
+ Busca una coincidencia solo al principio de la cadena. Si el patrón no coincide con el 
+ inicio de la cadena, re.match() devuelve None.
+"""
 # re.I hace que la busqueda ingnore entre mayúsculas y minúsculas
 match = re.match('Esta es la lección', my_string, re.I)
 print(match) # Imprime un objecto de coincidencia
@@ -36,6 +39,12 @@ footer()
 # search
 title('search')
 
+"""
+ Busca una coincidencia en cualquier parte de la cadena. Si el patrón se encuentra en 
+ cualquier posición dentro de la cadena, re.search() devuelve un objeto de coincidencia; 
+ de lo contrario, devuelve None.
+"""
+
 search = re.search('lección', my_string, re.I)
 print(search)
 
@@ -48,6 +57,17 @@ footer()
 # findall
 title('findall')
 
+"""
+Encuentra todas las coincidencias no superpuestas de un patrón en una cadena y las 
+devuelve como una lista de cadenas.
+
+re.findall(patron, cadena, flags=0)
+
+patron: La expresión regular que se va a buscar.
+cadena: La cadena en la que se realiza la búsqueda.
+flags: Flags opcionales que modifican el comportamiento de la búsqueda (ej. re.I para 
+ignorar mayúsculas/minúsculas).
+"""
 
 findall = re.findall('lección', my_string, re.I)
 print(findall)
@@ -57,12 +77,38 @@ footer()
 # split
 title('split')
 
+"""
+Divide una cadena en subcadenas usando un patrón como delimitador. Devuelve una lista de 
+las subcadenas.
+
+re.split(patron, cadena, maxsplit=0, flags=0)
+
+patron: La expresión regular que se utiliza como delimitador.
+cadena: La cadena que se va a dividir.
+maxsplit: Un entero opcional que especifica el número máximo de divisiones. Si es 0 (el 
+valor predeterminado), se realizan todas las divisiones posibles.
+flags: Flags opcionales.
+"""
+
 print(re.split(':',my_string))
 
 footer()
 
 # sub
 title('sub')
+
+"""
+Reemplaza todas las ocurrencias de un patrón en una cadena con una cadena de reemplazo.
+
+re.sub(patron, reemplazo, cadena, count=0, flags=0)
+
+patron: La expresión regular que se va a buscar.
+reemplazo: La cadena con la que se van a reemplazar las coincidencias.
+cadena: La cadena en la que se realiza el reemplazo.
+count: Un entero opcional que especifica el número máximo de reemplazos. Si es 0 (el 
+valor predeterminado), se reemplazan todas las ocurrencias.
+flags: Flags opcionales.
+"""
 
 print(re.sub('6', 'VI', my_string))
 print(re.sub('lección|Lección', 'clase', my_string))
